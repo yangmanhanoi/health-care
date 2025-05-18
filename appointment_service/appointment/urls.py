4037ab9b4683e1bd5a513084ceff97709870a21b
Nguyen Duc Thanh <nguyenducthanh240903@gmail.com>
2025-05-18 16:32:42 +0700

    Add appointment status updates and fields for diagnosis and conclusion

--- appointment_service/appointment/urls.py
+++ appointment_service/appointment/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('patient', patient_appointment_list_create, name='patient-list-create'),
+    path('<int:appointment_id>/check-in', check_in_appointment, name='check-in-appointment'),
+    path('<int:appointment_id>/diagnose', diagnose_appointment, name='diagnose-appointment'),
+    path('<int:appointment_id>/conclusion', conclude_appointment, name='conclude-appointment'),
 ]
