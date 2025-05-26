78da7fadfbbfa334e9205013e59ce18d32c98d24
HaPhuongAmelia <phuongnch03@gmail.com>
2025-05-26 17:20:29 +0700

    add cors

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from routes.forwarder import router
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app, resources={r"/svc-*/*": {"origins": "*"}}, supports_credentials=True)
 app.register_blueprint(router)
 
 if __name__ == "__main__":
--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
+     'corsheaders',
 
 ]
 
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'auth_service.urls'
 
